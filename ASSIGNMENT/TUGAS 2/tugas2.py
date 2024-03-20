f6c7e1fbd727643f25dec90972084f366ecac25e
mhdical1 <faisalhamzah770@gmail.com>
2024-03-20 11:46:07 +0700

    asd

---
+++ ASSIGNMENT/TUGAS 2/tugas2.py
@@ -0,0 +1,41 @@
+Keempat file jawaban pertanyaan berikut ditempatkan di akun GitHub pribadi masing-masing siswa. Kemudian sertakan link akun GitHub pada kolom submission di sini.
+
+Tulis program yang berbunyi berupa angka dan mencetak tanggal jumlah hari dari sekarang dengan format: Sabtu, 06 Februari 2021.
+
+Pada tugas pemrograman pertama bagian kedua ini, Anda harus menulis program yang membaca angka hingga -1 dimasukkan dan mencetak jumlah semua angka yang dimasukkan dalam format desimal dengan dua digit setelah desimal. Misalnya, jika pengguna memasukkan 5000 10 15 -1, program akan menampilkan 5025.00 (Setiap nomor akan dipisahkan dengan kereta kembali).
+
+Tulis kelas yang menghitung dan menyimpan tinggi dan berat seseorang dalam metrik. BMI dihitung menggunakan rumus ini:
+
+Berat/Tinggi ^ 2 - berat dalam kilogram dan tinggi badan dalam meter.
+
+Kelas harus memiliki dua properti bernama:
+
+Berat
+
+Tinggi
+
+Kelas harus memiliki satu metode yang mengembalikan nama desimal (tidak ada argumen yang diterima): BMI_Value.
+
+Tulis sebuah program yang mengambil nilai satu per satu hingga -1 terlihat menandai akhir daftar. Setiap nilai akan dipisahkan dengan tombol enter. Setelah selesai, keluarkan rata-rata (sebagai int) diikuti dengan nilai sesuai urutan yang Anda lihat.
+
+Contoh eksekusi akan terlihat seperti ini:
+
+Memasukkan:
+
+80
+
+75
+
+70
+
+-1
+
+Keluaran:
+
+75
+
+80
+
+75
+
+70
